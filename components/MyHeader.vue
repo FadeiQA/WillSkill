c72cfdeabcd9014e8aeff2af17bf9c94a8ed5fea
Михаил <fedorovskiy.m@yandex.ru>
2022-09-15 16:50:23 +0300

    Initial commit

---
+++ components/MyHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="menu">
+    <img src="/Vector.png" class="menu__logo">
+    <myMenu />
+    <div class="menu__basket">
+      <img src="/basket 1.png">
+      <div class="basket__items-carts">1</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderMenu",
+}
+</script>
+
+<style scoped>
+.menu {
+  position: fixed;
+  display: flex;
+  left: 0;
+  right: 0;
+  background: black;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1440px;
+  height: 80px;
+  color: white;
+  z-index: 999;
+  margin: auto;
+}
+
+.menu__logo {
+  height: 35px;
+  padding-left: 64px
+}
+
+.menu__basket {
+  padding-right: 88px;
+  position: relative
+}
+
+.basket__items-carts {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 82px;
+  bottom: 0;
+  background: orange;
+  border-radius: 300px;
+  width: 16px;
+  height: 16px;
+  color: black;
+  font-size: 12px;
+  line-height: 24px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,204 @@
+<template>
+  <div>
+    <section class="hero">
+      <img class="hero__nootris" src="/Mask_Group.png">
+      <h1 class="hero__title"> Активируй иммунитет!</h1>
+      <div class="hero__description"> Всего пять секунд в день помогут укрепить иммунитет <br> и повысить
+        защитные силы организма
+      </div>
+      <div class="hero__subtitle"> NOOTRIS ПОМОГАЕТ</div>
+      <div class="hero__subdescription"> Вашему организму во время пандемии <br> и сезонных простуд</div>
+    </section>
+    <section class="offer">
+      <div class="offer__title">
+        ПОЛУЧИ ЗАЩИТУ ИММУНИТЕТА <br>
+        <span class="offer__title_orange">СО СКИДКОЙ -15% ПЕРВЫМ!</span>
+      </div>
+      <div class="offer__price price">
+        <span class="price__before">&nbsp;750₽&nbsp; </span>
+        690₽
+      </div>
+    </section>
+    <section class="advantage">
+      <div>
+        <img src="/ginger.png" class="advantage__art">
+        <div class="advantage__title">Содержит</div>
+        <div class="advantage__body">имбирь</div>
+      </div>
+      <div>
+        <div>
+          <img src="/nootris.png" class="advantage__art_main">
+          <div class="advantage__title advantage__title_main"> + Бесплатная доставка</div>
+          <div class="advantage__body advantage__body_main"> Специальная цена</div>
+        </div>
+      </div>
+      <div>
+        <img src="/vorus.png" class="advantage__art">
+        <div class="advantage__title">Нейтрализует</div>
+        <div class="advantage__body">вирусы</div>
+      </div>
+    </section>
+    <section class="order">
+      <div class="order__btn btn"> Оформить заказ!</div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+
+.hero {
+  background: url("/bg.png");
+  position: relative;
+  padding: 130px 60px 96px;
+}
+
+.hero__title {
+  position: relative;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 144px;
+  line-height: 152px;
+  color: white;
+  text-transform: uppercase;
+  z-index: 100;
+  padding-bottom: 24px;
+  padding-top: 80px;
+}
+
+.hero__description {
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  margin-bottom: 120px;
+}
+
+.hero__subtitle {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 36px;
+  margin-bottom: 16px;
+}
+
+.hero__subdescription {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+}
+
+.hero__nootris {
+  position: absolute;
+  left: 620px;
+  top: 20px;
+  z-index: 0;
+}
+
+.offer {
+  margin-top: 110px;
+}
+
+.offer__title {
+  color: white;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 57px;
+  text-align: center;
+  margin-bottom: 85px;
+}
+
+.offer__title_orange {
+  color: #FCB500;
+}
+
+.advantage {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  text-align: center;
+  margin-bottom: 64px;
+  font-size: 18px;
+  line-height: 21px;
+}
+
+.offer__price {
+  padding-bottom: 59px;
+}
+
+.price {
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 48px;
+  line-height: 57px;
+}
+
+.price__before {
+  text-decoration: line-through;
+  color: #ADADAD;
+  font-size: 36px;
+  line-height: 43px;
+  padding-right: 30px;
+}
+
+.advantage__title_main {
+  font-size: 36px;
+  line-height: 43px;
+  margin-bottom: 8px;
+}
+
+.advantage__body_main {
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  color: #FCB500;
+}
+
+.advantage__title {
+  color: #FFFFFF;
+}
+
+.advantage__art {
+  margin-bottom: 78px;
+}
+
+.advantage__body {
+  color: #FCB500
+}
+
+.advantage__art_main {
+  margin-bottom: 45px;
+}
+
+.order {
+  text-align: center;
+  padding-bottom: 112px;
+}
+
+.order__btn {
+  margin: auto;
+}
+
+.btn {
+  display: inline-flex;
+  flex-grow: 0;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+  background: orange;
+  cursor: pointer;
+  min-height: 56px;
+  min-width: 256px;
+  border-radius: 7px;
+}
+
+</style>
+
